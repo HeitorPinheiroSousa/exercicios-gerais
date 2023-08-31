3e16f824dfd99160e76e8e0c7f3992d520f99935
HeitorPinheiroSousa <heitorps2020@gmail.com>
2023-08-31 10:37:25 -0300

    removido bibli_01.c

--- 03_bibliotecas/bibli_01/bibli_01.c
+++
@@ -1,9 +0,0 @@
-#include "Includes/esfera_utils.h"
-#include <stdio.h>
-
-int main(){
-    float r; scanf("%f",&r);
-    printf("Area: %.2f\n",calcula_area(r));
-    printf("Volume: %.2f\n",calcula_volume(r));
-return 0;
-}
