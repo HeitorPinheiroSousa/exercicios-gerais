138684dee8d61015fe02ddd957043a80928daa85
HeitorPinheiroSousa <heitorps2020@gmail.com>
2023-08-31 10:33:22 -0300

    script e padrao de pastas

---
+++ 03_bibliotecas/bibli_01/Respostas/Heitor/main.c
@@ -0,0 +1,9 @@
+#include "esfera_utils.h"
+#include <stdio.h>
+
+int main(){
+    float r; scanf("%f",&r);
+    printf("Area: %.2f\n",calcula_area(r));
+    printf("Volume: %.2f\n",calcula_volume(r));
+return 0;
+}
